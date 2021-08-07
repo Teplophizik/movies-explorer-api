1f03ba9aa343c4aced4e7eb9ebd2b6fbedec73be
Teplohizik <lucas_good.ini@mail.ru>
2021-08-07 15:02:21 +0300

    Revert "Создать API"
    This reverts commit 3dee7f67fd67f5b8622c52a1b5409e1eeff8df21.

--- helpers/urlValidator.js
+++
@@ -1,9 +0,0 @@
-const validator = require('validator');
-
-module.exports.urlValidator = (value, helpers) => {
-  const result = validator.isURL(value);
-  if (result) {
-    return value;
-  }
-  return helpers.error('any.invalid');
-};
